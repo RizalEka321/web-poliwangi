29a28e3c574b548098016faf06fb6c114f01888e
NaafiRamadhan <zidannaaafi@gmail.com>
2025-06-19 15:11:46 +0700

    test

--- src/components/layout/Navbar.vue
+++ src/components/layout/Navbar.vue
@@ -1,13 +1,4 @@
 <template>
-<<<<<<< HEAD
-  <nav class="bg-blue-600 text-white p-4 flex justify-between items-center">
-    <div class="font-bold text-xl">
-      <router-link to="/">Poliwangi</router-link>
-    </div>
-    <div class="space-x-4">
-      <router-link to="/" class="hover:text-gray-300">Home</router-link>
-      <router-link to="/about" class="hover:text-gray-300">About</router-link>
-=======
   <nav class="navbar bg-blue-700 text-white px-10 py-2 flex justify-between items-center">
     <div class="flex items-center space-x-3">
       <router-link to="/" class="flex items-center space-x-3">
@@ -25,7 +16,6 @@
       <router-link to="/about" class="hover:text-gray-300">PENDAFTARAN</router-link>
       <router-link to="/about" class="hover:text-gray-300">RISET & PENGABDIAN</router-link>
       <router-link to="/about" class="hover:text-gray-300">LAYANAN PUBLIK</router-link>
->>>>>>> a4c46a21f1488ce76cdcfd81219f77534e49d046
     </div>
   </nav>
 </template>
