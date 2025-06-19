a4c46a21f1488ce76cdcfd81219f77534e49d046
RizalEka321 <rizalekabp@gmail.com>
2025-06-19 14:24:08 +0700

    dua

--- src/components/layout/Navbar.vue
+++ src/components/layout/Navbar.vue
@@ -1,11 +1,21 @@
 <template>
-  <nav class="navbar text-white p-4 flex justify-between items-center">
-    <div class="font-bold text-xl">
-      <router-link to="/">Poliwangi</router-link>
+  <nav class="navbar bg-blue-700 text-white px-10 py-2 flex justify-between items-center">
+    <div class="flex items-center space-x-3">
+      <router-link to="/" class="flex items-center space-x-3">
+        <img src="/src/assets/icon/logo.png" alt="Logo" class="w-10 h-10 object-contain" />
+        <div class="leading-none">
+          <h1 class="text-sm">POLITEKNIK NEGERI</h1>
+          <h2 class="text-lg -mt-1">BANYUWANGI</h2>
+        </div>
+      </router-link>
     </div>
-    <div class="space-x-4">
-      <router-link to="/" class="hover:text-gray-300">Home</router-link>
-      <router-link to="/about" class="hover:text-gray-300">About</router-link>
+
+    <div class="space-x-5 text-xs">
+      <router-link to="/" class="hover:text-gray-300">PROFIL</router-link>
+      <router-link to="/" class="hover:text-gray-300">AKADEMIK</router-link>
+      <router-link to="/about" class="hover:text-gray-300">PENDAFTARAN</router-link>
+      <router-link to="/about" class="hover:text-gray-300">RISET & PENGABDIAN</router-link>
+      <router-link to="/about" class="hover:text-gray-300">LAYANAN PUBLIK</router-link>
     </div>
   </nav>
 </template>
