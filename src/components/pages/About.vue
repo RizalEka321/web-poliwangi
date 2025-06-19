87ffd4a1bfe7383a4c6cc8564a45ea25ea3f2e19
NaafiRamadhan <zidannaaafi@gmail.com>
2025-06-19 14:00:36 +0700

    first

---
+++ src/components/pages/About.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="p-10">
+    <h1 class="text-4xl font-bold text-purple-600">About Page</h1>
+    <p class="mt-4">This is About page using Vue Router and Tailwind.</p>
+  </div>
+</template>
---
+++ src/components/pages/Home.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="p-10">
+    <h1 class="text-4xl font-bold text-blue-600">Home Page</h1>
+    <button class="mt-4 px-4 py-2 bg-green-500 text-white rounded">Load Data</button>
+  </div>
+</template>
